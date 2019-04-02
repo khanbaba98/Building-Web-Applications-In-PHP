827204cf27f08b0e290cc185184b6945ce128ad5
Rahil Khan <47527462+khanbaba98@users.noreply.github.com>
2019-04-02 15:32:27 +0530

    Added course content (assignments)
    Full upload of assignments

---
+++ Week 5/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Rahil Khan PHP</title>
+	</head>
+	<body>
+		<h1>Rahil Khan PHP</h1>
+
+		<pre>ASCII ART (First letter of the Name 'Rahil' - 'R'):
+
+		*******
+	**   **
+	**   **
+	******
+	**  **
+	**   **
+	**    **
+	**     **
+		</pre>
+
+		<p>
+			The SHA256 hash of "Rahil Khan" is <?php print hash('sha256', 'Rahil Khan'); ?>
+		</p>
+	
+	</body>
+</html>
